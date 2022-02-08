9b0b8cd3a42e75c85885edf7ee59604cc54f0414
Jinsuk.Choi <jinsuk@codemind.com>
2022-02-08 15:31:56 +0900

    initialize springboot sample project

---
+++ gitsample/src/main/java/com/codemind/gitsample/GitsampleApplication.java
@@ -0,0 +1,13 @@
+package com.codemind.gitsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitsampleApplication.class, args);
+	}
+
+}
---
+++ gitsample/src/test/java/com/codemind/gitsample/GitsampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codemind.gitsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitsampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
