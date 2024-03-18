397276fb01b1b19333821b5084928195c514a66b
Jinsuk.Choi <jinsuk@codemind.co.kr>
2024-03-18 13:45:55 +0900

    feature: FileUtils isWindows...2
    - FileUtils.java added 2

--- gitsample/src/main/java/com/codemind/gitsample/FileUtils.java
+++ gitsample/src/main/java/com/codemind/gitsample/FileUtils.java
@@ -21,11 +21,6 @@ public class FileUtils {
         return osName.contains("win");
     }
 
-    public static boolean isWindows2() {
-        String osName = System.getProperty("os.name").toLowerCase();
-        return osName.contains("win");
-    }
-
     public static String getPlatformPath(String path) {
         String gitConfigPath = path;
         if(FileUtils.isWindows()) {
